c466b46dc165fa2377896a9421cb7ec9335cc57f
vijai <vijai.djearam@gmail.com>
2023-10-27 15:34:21 +0200

    Update startup_script.ps1

--- startup_script.ps1
+++ startup_script.ps1
@@ -1,6 +1,6 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 New-Item -ItemType Directory -Path C:\Users\WDAGUtilityAccount\Desktop\chocolatey-export-assets
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/vijaidjearam/chocolatey-export-assets/main/chocolatey-export-assets.py" -OutFile "C:\Users\WDAGUtilityAccount\Desktop\chocolatey-export-assets\extract-macadress-servicetag.py"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/vijaidjearam/chocolatey-export-assets/main/chocolatey-export-assets.py" -OutFile "C:\Users\WDAGUtilityAccount\Desktop\chocolatey-export-assets\chocolatey-export-assets.py"
 choco install -y python
 python.exe -m pip install --upgrade pip
 pip install requests
